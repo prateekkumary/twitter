4b4cd6ee76714af32131e732ea67c364b7cb1dad
Prateek Yadav <prateekkumary73@gmail.com>
2025-06-11 08:00:06 +0530

    feat: add TweetCard component with user info, image, and action buttons

---
+++ src/Components/HomeSection/TweetCard.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+import RepeatIcon from '@mui/icons-material/Repeat';
+import { Avatar } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Menu from '@mui/material/Menu';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+
+
+
+
+
+
+const TweetCard = () => {
+
+    const naviagate = useNavigate();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleDeleteTweet = () => {
+        console.log("Delete Tweet");
+        handleClose();
+    }
+    const handleOpenReplyModel = () => {
+        console.log("Open Reply Model");
+    }
+    const handleCreateRetweet = () => {
+        console.log("Create Retweet");
+    }
+
+    const handleLikeTweet = () => {
+        console.log("Like Tweet");
+    }
+
+
+
+
+
+
+    return (
+        <div>
+            {/* <div className='flex items-center font-semibold text-gray-800 py-2'>
+            <RepeatIcon/>
+            <p>You Retweet</p>
+
+        </div> */}
+            <div className='flex space-x-5'>
+                <Avatar onClick={() => naviagate(`/profile/${6}`)} alt="username" src="https://pbs.twimg.com/profile_images/1476982902701244417/GiwXfZy5_400x400.jpg" className='cursor-pointer' />
+                <div className='w-full'>
+                    <div className='flex justify-between items-center'>
+                        <div className='flex cursor-pointer items-center space-x-2'>
+                            <span className='font-semibold '>
+                                Prateek Yadav
+                            </span>
+                            <span className='text-gray-600'>
+                                @utopians .2m
+                            </span>
+                            <VerifiedIcon className='text-blue-500 ml-2 w-5 h-5' />
+
+                        </div>
+                        <div>
+                            <Button
+                                id="basic-button"
+                                aria-controls={open ? 'basic-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                                onClick={handleClick}
+                            >
+                                <MoreHorizIcon />
+                            </Button>
+                            <Menu
+                                id="basic-menu"
+                                anchorEl={anchorEl}
+                                open={open}
+                                onClose={handleClose}
+                                slotProps={{
+                                    list: {
+                                        'aria-labelledby': 'basic-button',
+                                    },
+                                }}
+                            >
+                                <MenuItem onClick={handleDeleteTweet}>Delete</MenuItem>
+                                <MenuItem onClick={handleDeleteTweet}>Edit</MenuItem>
+
+                            </Menu>
+                        </div>
+
+                    </div>
+                    <div className='mt-2'>
+                        <div className='cursor-pointer'>
+                            <p className='mb-2 p-0'> This is nice projrct. this is springboot full stack prject</p>
+                            <img className='w-[28rem] border border-gray-400 p-5 rounded-md' src="https://pbs.twimg.com/profile_images/1476982902701244417/GiwXfZy5_400x400.jpg" alt="" />
+
+                        </div>
+                        <div className='py-5 flex flex-wrap justify-between items-center'>
+                            <div className='space-x-3 flex items-center text-zinc-600'>
+                                <ChatBubbleOutlineIcon className='cursor-pointer' onClick={handleOpenReplyModel} />
+                                <p>43</p>
+
+                            </div>
+                            <div className={`${true ? "text-pink-600" : "text-gray-600"} space-x-3 flex items-center`}>
+                                <RepeatIcon onClick={handleCreateRetweet} className='cursor-pointer' />
+                                <p>53</p>
+
+                            </div>
+                            <div className={`${true ? "text-pink-600" : "text-gray-600"} space-x-3 flex items-center`}>
+                                {true ? <FavoriteIcon onClick={handleLikeTweet} className='cursor-pointer' /> : <FavoriteBorderIcon onClick={handleLikeTweet} className='cursor-pointer' />}
+                                <p>53</p>
+
+                            </div>
+                            <div className='space-x-3 flex items-center text-zinc-600'>
+                                <BarChartIcon className='cursor-pointer' onClick={handleOpenReplyModel} />
+                                <p>439</p>
+                            </div>
+                            <div className='space-x-3 flex items-center text-zinc-600'>
+                                <FileUploadIcon className='cursor-pointer' onClick={handleOpenReplyModel} />
+                                
+                                </div>
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TweetCard
